0490b048a0c83e2258b5ac235a5d35d6a770c76b
Jose Bocanegra <j.bocanegra@uniandes.edu.co>
2022-03-23 12:51:02 -0500

    Update Jenkinsfile

--- update.js
+++ update.js
@@ -4,7 +4,7 @@ let fs = require("fs");
 
 const projects = [
   /*{ name: "202212_Equipo01" },*/
-  { name: "202212_Equipo02" },
+  { name: "202212_Equipo03" },
   /*{ name: "202212_Equipo03" },
   { name: "202212_Equipo04" },
   { name: "202212_Equipo05" }
