01601629352b4a9875492286b4bef772456b8c7b
José Bocanegra <j.bocanegra@uniandes.edu.co>
2023-02-28 14:01:44 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-const baseUrl = 'https://misw4104-api.herokuapp.com/api/';
+const baseUrl = 'https://desarrollow.virtual.uniandes.edu.co/bookstore/';
 
 export const environment = {
   production: false,
